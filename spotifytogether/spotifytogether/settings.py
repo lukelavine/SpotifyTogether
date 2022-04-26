4af823bc2e0e43abbf1f21b9a3359349977fc53b
Luke Lavine <luke@lukelavine.com>
2022-04-26 08:07:15 +0000

    Setting configuration for server

--- spotifytogether/spotifytogether/settings.py
+++ spotifytogether/spotifytogether/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-mq27fiet7js98^muzm5i%gmj7=m((wa=lazp+^*x3ikqqh=uif
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        '52.12.104.46',
+        '127.0.0.1',
+]
 
 
 # Application definition
@@ -119,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
